45bcff239ddafb098bcca06d7c361a819fa4ecef
Doosik Kim <doosik71@gmail.com>
2022-11-13 17:02:04 +0900

    Add system.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use bevy::prelude::*;
 
+fn hello_world() {
+    println!("hello world!");
+}
+
 fn main() {
     App::new()
-        .add_plugin(CorePlugin::default())
+        .add_system(hello_world)
         .run();
 }
